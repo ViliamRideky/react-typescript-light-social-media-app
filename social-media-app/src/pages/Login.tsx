1f4bccef2aa4dada6896c3f1d5ca0e859dc46c0d
ViliamRideky <rideky.viliam@gmail.com>
2024-08-27 19:57:48 +0200

    changed design for login

--- social-media-app/src/pages/Login.tsx
+++ social-media-app/src/pages/Login.tsx
@@ -1,6 +1,7 @@
 import { auth, provider } from '../config/firebase'
 import { signInWithPopup } from 'firebase/auth'
 import { useNavigate } from 'react-router-dom'
+import googleLogo from '../images/google_logo.png';
 
 export const Login = () => {
 
@@ -12,9 +13,14 @@ export const Login = () => {
     }
 
     return (
-        <div>
-            <p>Sign In With Google To Continue</p>
-            <button onClick={signInWithGoogle}>Sign In With Google</button>
+        <div className="login-container">
+            <div className="login-box">
+                <h2>Sign In to Continue</h2>
+                <p>Use your Google account to sign in and access the application.</p>
+                <button onClick={signInWithGoogle} className="google-signin-button">
+                    <img src={googleLogo} alt="Google Logo" className="google-logo" />
+                </button>
+            </div>
         </div>
-    )
+    );
 }
